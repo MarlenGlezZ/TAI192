48813450559f36ddfed81e30a060e98a054787d5
MarlenGlezZ <122043522@upq.edu.mx>
2025-01-29 19:51:27 -0600

    Práctica 2-1
    Creación de endpoints con SwaggerUI

--- FAST/main.py
+++ FAST/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 
-app = FastAPI() 
+app = FastAPI(
+    title='Mi Primer API 192',
+    description='Bienvenide al intento de hacer APIs',
+    version='1.0.1'
+    
+) 
 
 #Endpoint Home
-@app.get('/')
+@app.get('/',tags=['Hola Mundo'])#Asiganación del nombre del titulo
 def home():
     return {'Hello': 'world FastAPI'}
+
+#Endpoint promedio
+@app.get('/promedio', tags=['Mi calificación TAI'])
+def promedio():
+    return 5.2
+
+#Endpoint parametro obligatorio
+@app.get('/usuario/{id}', tags=['Parámetro Obligatorio'])
+def consultaUsuario(id:int):
+    #conexion a la bd
+    #consulta
+    return {'Se encontró el usuario':id}
+
+
